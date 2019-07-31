a2eddabfba709bd81d6c0cace7fd12db4e75b228
Buhtoyarov Artem <users1100@mail.ru>
2019-07-31 12:02:41 +0300

    fixed logging when rescue

--- lib/sprockets/exporters/webp_exporter.rb
+++ lib/sprockets/exporters/webp_exporter.rb
@@ -7,6 +7,7 @@ module Sprockets
     class WebpExporter < Exporters::Base
       def setup
         @webp_target = "#{target}.webp"
+        @logger = Logger.new($stderr)
       end
 
       def skip?(logger)
@@ -21,8 +22,8 @@ module Sprockets
 
       def call
         ::WebP.encode(target, @webp_target, Sprockets::WebP.encode_options)
-      rescue WebP::EncoderError
-        logger.warn "Skipping #{@webp_target}, converter error"
+      rescue ::WebP::EncoderError
+        @logger.warn "Skipping #{@webp_target}, converter error"
       end
     end
   end
--- lib/sprockets/webp/version.rb
+++ lib/sprockets/webp/version.rb
@@ -2,6 +2,6 @@
 
 module Sprockets
   module WebP
-    VERSION = '0.1.0.rc3'
+    VERSION = '0.1.0.rc4'
   end
 end
